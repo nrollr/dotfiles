3509f34e51ff8af6da95bfb4427032d68aa7bee5
nrollr <nrollr@users.noreply.github.com>
2021-11-04 11:47:03 +0100

    work in progress

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,6 @@ exit
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/shell/default.zshrc $HOME/.zshrc
 
-# Load Terminal profile
-open ~/.dotfiles/shell/nord.terminal
-defaults write com.apple.terminal "Default Window Settings" "nord.terminal"
-
 # Install all brew packages with bundle
 echo "Installing brew packages"
 
@@ -22,13 +18,16 @@ brew tap homebrew/bundle
 brew bundle --file=$HOME/.dotfiles/homebrew/brewfile
 echo "Packages installed!"
 
+# Symlink Terminal profile from .dotfiles/shell
+rm -rf $HOME/Library/Preferences/com.apple.Terminal.plist
+ln -s $HOME/.dotfiles/shell/com.apple.Terminal.plist $HOME/Library/Preferences/com.apple.Terminal.plist
+
 # Clone Github repositories
 echo "Cloning github repositories"
 
 cd $HOME/Downloads && mkdir Github && cd Github
 sh $HOME/.dotfiles/github/setup.sh
 echo "Github repositories cloned!"
-
 
 # Set macOS preferences
 # source $HOME/.dotfiles/macos/setup.sh
